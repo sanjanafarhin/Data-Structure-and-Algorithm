5cd7c05766e845b375d5f0c13214ec19f29d6fbb
Sanjana Farhin Astha <sastha2420212@bscse.uiu.ac.bd>
2026-04-06 20:22:53 +0000

    d&c entry

---
+++ Merge_sort_vector/merge_sort.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void Merge (vector<int>&datas,int low,int high,int mid)
+{
+   vector<int>temp(high-low+1);
+   int temp_index=0;
+   int i=low;
+   int j=mid+1;
+
+   while(i<=mid && j<=high)
+   {
+    if(datas[i]<datas[j])
+    {
+        temp[temp_index++]=datas[i++];
+    }
+    else
+    {
+       temp[temp_index++]=datas[j++]; 
+    }
+   }
+
+   for(;i<=mid;)
+   {
+    temp[temp_index++]=datas[i++];
+   }
+
+   for(;j<=high;)
+   {
+    temp[temp_index++]=datas[j++];
+   }
+
+   temp_index=0;
+
+   for(int a=low;a<high;a++)
+   {
+    datas[a]=temp[temp_index++];
+   }
+
+
+}
+
+void Merge_sort(vector<int>&Arr, int low, int high)
+{
+    if (low < high)
+    {
+
+        /*
+        STEP 1.
+
+        Divide untill n=1;
+
+        Level 1 → 8 elements
+        Level 2 → 4 + 4
+        Level 3 → 2 + 2 + 2 + 2
+        Level 4 → 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+
+        */
+        int mid = (low + high) / 2;
+        Merge_sort(Arr, low, mid);
+        Merge_sort(Arr, mid + 1, high);
+
+        // conquer and combine
+        Merge(Arr, low, high,mid);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "how much indexes:" << endl;
+    cin >> n;
+    vector<int>data(n);
+    cout << "write a set of array:" << endl;
+    for (int &number : data)
+    {
+        cin >> number;
+    }
+    Merge_sort(data, 0, n - 1);
+
+    cout << "After sorting" << endl;
+
+    for (int index : data)
+    {
+        cout << index << ",";
+    }
+
+    return 0;
+}
