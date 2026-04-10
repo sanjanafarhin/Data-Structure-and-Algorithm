596f403febca7317827c36fcf63f366fe55c3e18
Sanjana Farhin Astha <sastha2420212@bscse.uiu.ac.bd>
2026-04-10 11:51:41 +0000

    divide and conquer entry

--- max sum sub array/maximum_subarray_sum.cpp
+++ max sum sub array/maximum_subarray_sum.cpp
@@ -11,42 +11,42 @@ struct sum_index
 };
 
 sum_index crossing_sum(vector<int> datas, int low, int high, int mid)
-{   sum_index res;
+{
+    sum_index res;
     int left = mid;
     int right = mid + 1;
     int left_sum = 0;
     int right_sum = 0;
-    int high_left_sum = INT_MIN; //for -infinity to + infinity
+    int high_left_sum = INT_MIN; // to avoid any problem while having negative number as large
     int high_right_sum = INT_MIN;
-    
-    //left sum
-    while (left >= 0)
+
+    // left sum
+    while (left >= low)
     {
-       left_sum = left_sum + datas[left];
-       
-        if(left_sum > high_left_sum)
+        left_sum = left_sum + datas[left];
+
+        if (left_sum > high_left_sum)
         {
-            high_left_sum=left_sum;
-            res.left_index=left;
+            high_left_sum = left_sum;
+            res.left_index = left;
         }
-        
+
         left--;
     }
 
-
-    //right sum
+    // right sum
     while (right <= high)
     {
         right_sum = right_sum + datas[right];
-       if(right_sum > high_right_sum)
+        if (right_sum > high_right_sum)
         {
-            high_right_sum=right_sum;
-            res.right_index=right;
+            high_right_sum = right_sum;
+            res.right_index = right;
         }
         right++;
     }
 
-     res.high_sum = high_left_sum + high_right_sum;
+    res.high_sum = high_left_sum + high_right_sum;
     return res;
 }
 
@@ -55,19 +55,19 @@ sum_index Maximum_sub_array_sum(vector<int> datas, int low, int high)
     sum_index res;
     int cross;
     if (low == high)
-    {   
-        res.high_sum=datas[low];
-        res.left_index=low;
-        res.right_index=low;
+    {
+        res.high_sum = datas[low];
+        res.left_index = low;
+        res.right_index = low;
         return res;
     }
     else
     {
         int mid = (high + low) / 2;
-        sum_index left = Maximum_sub_array_sum(datas, low, mid); //left cross sum
-        sum_index right = Maximum_sub_array_sum(datas, mid + 1, high); //right cross sum
-        sum_index crossing_sums = crossing_sum(datas, low, high, mid); //cross sum
- 
+        sum_index left = Maximum_sub_array_sum(datas, low, mid);       // left cross sum
+        sum_index right = Maximum_sub_array_sum(datas, mid + 1, high); // right cross sum
+        sum_index crossing_sums = crossing_sum(datas, low, high, mid); // cross sum
+
         if (left.high_sum > crossing_sums.high_sum && left.high_sum > right.high_sum)
         {
             return left;
@@ -96,5 +96,5 @@ int main()
     }
     sum_index maximum_sum_index = Maximum_sub_array_sum(datas, 0, num - 1);
     cout << "Highest sum Score: " << maximum_sum_index.high_sum << endl;
-    cout << "index range : (" << maximum_sum_index.left_index <<","<< maximum_sum_index.right_index << ")";
+    cout << "index range : (" << maximum_sum_index.left_index << "," << maximum_sum_index.right_index << ")";
 }
