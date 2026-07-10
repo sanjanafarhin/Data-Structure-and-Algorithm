7ada2b36ab173ace39943bd218875db1c41a47c4
sanjanafarhin <sastha2420212@bscse.uiu.ac.bd>
2026-07-10 16:45:06 +0600

    your commit message

--- Linked  List/createnode.cpp
+++ Linked  List/createnode.cpp
@@ -4,13 +4,21 @@
 struct listnode
 {
     int data;
-    listnode* next_address;
+    listnode* next_address; /*<-- pointing the address for next data */
 
 };
 
 listnode* CREATE (int data)
 {
     listnode* Node = (listnode*)malloc(sizeof(listnode));
+    
+    /*
+    -> we are returning listnode as pointer because 
+    we are pointig address of the next data
+    -> in default malloc's data type is void so we are typecasting the malloc as the referance of temporary node datatype
+    -> we are asigning the size od malloc dynamically so we are using the struct size;
+    */
+
     Node->data = data;
     Node->next_address=NULL;
 
---
+++ Linked  List/insert_node.cpp
@@ -0,0 +1,71 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+struct listnode
+{
+    int data;
+    listnode *next_address;
+};
+
+/*Trace pointer initialization*/
+listnode *head = NULL;
+listnode *tail = NULL;
+
+listnode *CREATE(int data)
+{
+    listnode *Node = (listnode *)malloc(sizeof(listnode *));
+    Node->data = data;
+    Node->next_address = NULL;
+
+    return Node;
+}
+
+void insertlast(int data)
+{
+    /*
+    CASES to handle:
+
+    -> what if head is empty (head=tail=NULL)
+
+    1.create temporary Linklist [Node A | X]
+
+    2. assign head and tail both are Node A
+
+       [Node A | X]
+        head-tail
+
+    ->if head is not empty
+
+    [NODE A| X ]--> linklist
+       tail
+     1.create temporary Linklist [Node B | X]
+
+     2.connect the temporary linklist with the address of last tail
+
+       [NODE A|]->[Node B | X]
+
+     3. Assign the new data as the tail
+
+       [NODE A|]->[Node B | X]
+                      Tail
+
+    */
+
+    if (head == NULL)
+    {
+        listnode *tempnode = CREATE(data);
+        head = tempnode;
+        tail = tempnode;
+    }
+    else
+    {
+        // step 1
+        listnode *tempnode = CREATE(data);
+
+        // step 2
+        tail->next_address = tempnode;
+
+        // step 3
+        tail = tempnode;
+    }
+}
